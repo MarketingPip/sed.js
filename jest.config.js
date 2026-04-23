1abbce15961b6a9100e5c8759113462c4d8159ee
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-04-23 14:01:17 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,3 @@
 export default {
-  transform: {
-    "^.+\\.[tj]sx?$": "babel-jest",  // Use babel-jest to transpile all JavaScript/TypeScript files
-  },
   testEnvironment: "node",  // Node environment for testing
 };
