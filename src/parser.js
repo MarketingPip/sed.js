99c4be2673dae5fe40676052c8d1cf08ac375a2b
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-04-23 18:58:47 -0400

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,4 +1,4 @@
-import {SedLexer} from "./lexer.js";
+import {SedLexer, SedTokenType} from "./lexer.js";
 
 export class SedParser {
   constructor(scripts, extendedRegex = false) { this.scripts = scripts; this.extendedRegex = extendedRegex; this.tokens = []; this.pos = 0; }
