1e016a5ad309f2951b609712ee17d41e9fd766c0
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-04-23 23:07:40 -0400

    Update main.js

--- website/js/main.js
+++ website/js/main.js
@@ -1,5 +1,4 @@
 import "../css/styles.css";
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../pages/index.html';
 function copyToClipboard() {
     const text = document.getElementById('installText').innerText;
